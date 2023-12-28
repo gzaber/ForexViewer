d0bf55ea5fc4bfb271386813afe8dadc348798f9
Grzesiek <gzaber@tlen.pl>
2023-12-28 23:40:19 +0100

    feat: add favorites local data source

---
+++ app/src/main/java/com/gzaber/forexviewer/data/source/local/FavoriteDao.kt
@@ -0,0 +1,20 @@
+package com.gzaber.forexviewer.data.source.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(favorite: FavoriteEntity)
+
+    @Delete
+    suspend fun delete(favorite: FavoriteEntity)
+
+    @Query("SELECT * FROM favorites")
+    fun loadAll(): Flow<List<FavoriteEntity>>
+}
