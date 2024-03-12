9d181b7bf4e6e5827ff0753aee8c41bf946480f2
Grzesiek <gzaber@tlen.pl>
2024-03-12 19:20:07 +0100

    refactor: use isLoading instead of ui status

--- app/src/main/java/com/gzaber/forexviewer/ui/forexpairs/ForexPairsViewModel.kt
+++ app/src/main/java/com/gzaber/forexviewer/ui/forexpairs/ForexPairsViewModel.kt
@@ -5,19 +5,17 @@ import androidx.lifecycle.viewModelScope
 import com.gzaber.forexviewer.data.repository.favorites.FavoritesRepository
 import com.gzaber.forexviewer.data.repository.forexdata.ForexDataRepository
 import com.gzaber.forexviewer.ui.util.model.UiForexPair
+import com.gzaber.forexviewer.ui.util.model.toFavorite
 import com.gzaber.forexviewer.ui.util.model.toUiModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-enum class ForexPairsStatus {
-    LOADING, SUCCESS, FAILURE
-}
 
 enum class ForexGroup(val value: String) {
     ALL("All"),
@@ -28,11 +26,11 @@ enum class ForexGroup(val value: String) {
 }
 
 data class ForexPairsUiState(
-    val status: ForexPairsStatus = ForexPairsStatus.LOADING,
-    val forexPairs: List<UiForexPair> = listOf(),
+    val isLoading: Boolean = true,
+    val uiForexPairs: List<UiForexPair> = listOf(),
     val searchText: String = "",
     val group: ForexGroup = ForexGroup.MAJOR,
-    val failureMessage: String = ""
+    val failureMessage: String? = null
 )
 
 @HiltViewModel
@@ -41,47 +39,46 @@ class ForexPairsViewModel @Inject constructor(
     private val favoritesRepository: FavoritesRepository
 ) : ViewModel() {
 
-    private val _forexGroup = MutableStateFlow(ForexGroup.ALL)
-    private val _searchText = MutableStateFlow("")
+    private val _uiState = MutableStateFlow(ForexPairsUiState())
+    val uiState = _uiState.asStateFlow()
 
-    val uiState = combine(
-        forexDataRepository.fetchAllForexPairs(),
-        favoritesRepository.loadAllFavorites(),
-        _forexGroup,
-        _searchText
-    ) { forexPairs, favorites, group, searchText ->
-        val uiForexPairs = forexPairs.map { forexPair ->
-            forexPair.toUiModel(
-                isFavorite = favorites.any { favorite ->
-                    favorite.symbol == forexPair.symbol
-                },
-                favoriteId = favorites.find { it.symbol == forexPair.symbol }?.id
-            )
-        }.filter {
-            (it.group == group.value) || (group == ForexGroup.ALL)
-        }.filter {
-            it.symbol.contains(_searchText.value, ignoreCase = true) ||
-                    it.base.contains(_searchText.value, ignoreCase = true) ||
-                    it.quote.contains(_searchText.value, ignoreCase = true)
+    init {
+        viewModelScope.launch {
+            combine(
+                forexDataRepository.fetchAllForexPairs(),
+                favoritesRepository.loadAllFavorites(),
+                _uiState
+            ) { forexPairs, favorites, uiState ->
+                forexPairs.map { forexPair ->
+                    forexPair.toUiModel(
+                        isFavorite = favorites.any { favorite ->
+                            favorite.symbol == forexPair.symbol
+                        },
+                        favoriteId = favorites.find { it.symbol == forexPair.symbol }?.id
+                    )
+                }.filter {
+                    (it.group == uiState.group.value) || (uiState.group == ForexGroup.ALL)
+                }.filter {
+                    it.symbol.contains(uiState.searchText, ignoreCase = true) ||
+                            it.base.contains(uiState.searchText, ignoreCase = true) ||
+                            it.quote.contains(uiState.searchText, ignoreCase = true)
+                }
+            }
+                .catch { e ->
+                    _uiState.update {
+                        it.copy(failureMessage = e.message)
+                    }
+                }
+                .collect { uiForexPairs ->
+                    _uiState.update {
+                        it.copy(
+                            isLoading = false,
+                            uiForexPairs = uiForexPairs
+                        )
+                    }
+                }
         }
-        ForexPairsUiState(
-            status = ForexPairsStatus.SUCCESS,
-            forexPairs = uiForexPairs,
-            searchText = searchText,
-            group = group
-        )
     }
-        .catch {
-            ForexPairsUiState(
-                status = ForexPairsStatus.FAILURE,
-                failureMessage = it.message ?: ""
-            )
-        }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5000),
-            initialValue = ForexPairsUiState()
-        )
 
     fun toggleFavorite(uiForexPair: UiForexPair) {
         viewModelScope.launch {
@@ -92,20 +89,34 @@ class ForexPairsViewModel @Inject constructor(
                     favoritesRepository.insertFavorite(uiForexPair.toFavorite())
                 }
             } catch (e: Throwable) {
-                // TODO: do something
+                _uiState.update {
+                    it.copy(failureMessage = e.message)
+                }
             }
         }
     }
 
     fun onForexGroupSet(group: String) {
-        _forexGroup.value = ForexGroup.valueOf(group)
+        _uiState.update {
+            it.copy(group = ForexGroup.valueOf(group))
+        }
     }
 
     fun onSearchTextChanged(text: String) {
-        _searchText.value = text
+        _uiState.update {
+            it.copy(searchText = text)
+        }
     }
 
     fun onSearchTextCleared() {
-        _searchText.value = ""
+        _uiState.update {
+            it.copy(searchText = "")
+        }
+    }
+
+    fun snackbarMessageShown() {
+        _uiState.update {
+            it.copy(failureMessage = null)
+        }
     }
 }
