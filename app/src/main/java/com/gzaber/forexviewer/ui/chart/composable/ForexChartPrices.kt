c8b1cc57f795a090e0a81d6b7bd64f0381b31b58
Grzesiek <gzaber@tlen.pl>
2024-03-14 16:55:48 +0100

    refactor: use drawLine instead of hyphen as price separator

--- app/src/main/java/com/gzaber/forexviewer/ui/chart/composable/ForexChartPrices.kt
+++ app/src/main/java/com/gzaber/forexviewer/ui/chart/composable/ForexChartPrices.kt
@@ -6,6 +6,8 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextMeasurer
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.unit.dp
@@ -17,37 +19,44 @@ fun ForexChartPrices(
     chartMax: Double,
     heightUsed: Double,
     modifier: Modifier = Modifier,
-    pricesNumber: Int = 8
+    pricesNumber: Int = 8,
+    priceSeparatorWidth: Float = 10f,
+    textMeasurer: TextMeasurer = rememberTextMeasurer()
 ) {
-
-    val textMeasurer = rememberTextMeasurer()
 
     Canvas(
         modifier = modifier
-            .width(50.dp)
+            .width(55.dp)
             .fillMaxHeight()
     ) {
         val ratio = size.height * heightUsed / (chartMax - chartMin)
         val step = (chartMax - chartMin) / (pricesNumber - 1)
 
         repeat(pricesNumber + 1) {
-
             val measuredText = textMeasurer.measure(
-                text = "- ${
-                    String.format(Locale.ENGLISH, "%.4f", chartMax - (it * step))
-                }"
+                String.format(Locale.ENGLISH, "%.4f", chartMax - (it * step))
             )
-            val textOffset = Offset(
+            val baseOffset = Offset(
                 x = 0f,
                 y = (it * step * ratio + size.height * ((1 - heightUsed) / 2)).toFloat() - measuredText.size.height / 2
             )
 
+            drawLine(
+                color = Color.Gray,
+                strokeWidth = 3f,
+                start = baseOffset.copy(
+                    y = baseOffset.y + measuredText.size.height / 2
+                ),
+                end = Offset(
+                    x = priceSeparatorWidth,
+                    y = baseOffset.y + measuredText.size.height / 2
+                )
+            )
+
             drawText(
                 textLayoutResult = measuredText,
-                topLeft = textOffset
+                topLeft = baseOffset.copy(x = priceSeparatorWidth)
             )
         }
-
-
     }
 }
