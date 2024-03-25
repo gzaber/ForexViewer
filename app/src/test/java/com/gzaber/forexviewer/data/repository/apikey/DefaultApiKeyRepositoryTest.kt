2d385345163c656079ee855034a8e2a37866a25c
Grzesiek <gzaber@tlen.pl>
2024-03-25 14:18:43 +0100

    test: add api key repository tests

---
+++ app/src/test/java/com/gzaber/forexviewer/data/repository/apikey/DefaultApiKeyRepositoryTest.kt
@@ -0,0 +1,50 @@
+package com.gzaber.forexviewer.data.repository.apikey
+
+import com.gzaber.forexviewer.data.source.local.FakeDataStore
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Test
+
+
+class DefaultApiKeyRepositoryTest {
+
+    private lateinit var localDataSource: FakeDataStore
+    private lateinit var repository: DefaultApiKeyRepository
+
+    private fun createRepository(
+        initialData: String? = null,
+        shouldThrow: Boolean = false
+    ) {
+        localDataSource = FakeDataStore(initialData = initialData, shouldThrow = shouldThrow)
+        repository = DefaultApiKeyRepository(dataStore = localDataSource)
+    }
+
+    @Test
+    fun loadApiKey_dataNotFound_emitsDefaultValue() = runTest {
+        createRepository()
+        val result = repository.loadApiKey().first()
+        assert(result == "demo")
+    }
+
+    @Test
+    fun loadApiKey_emitsFromLocalDataSource() = runTest {
+        createRepository(initialData = "123apikey")
+        val result = repository.loadApiKey().first()
+        assert(result == "123apikey")
+    }
+
+    @Test
+    fun loadApiKey_localDataSourceThrows_emitsDefaultValue() = runTest {
+        createRepository(initialData = "123apikey", shouldThrow = true)
+        val result = repository.loadApiKey().first()
+        assert(result == "demo")
+    }
+
+    @Test
+    fun saveApiKey_savesToLocalDataSource() = runTest {
+        createRepository()
+        repository.saveApiKey("123apikey")
+        val result = repository.loadApiKey().first()
+        assert(result == "123apikey")
+    }
+}
