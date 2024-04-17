225af565692d73f0bb7cc3a21a1130f2ae7d7b2e
Grzesiek <gzaber@tlen.pl>
2024-04-17 22:05:46 +0200

    refactor: add test tag

--- app/src/main/java/com/gzaber/forexviewer/ui/util/composable/LoadingBox.kt
+++ app/src/main/java/com/gzaber/forexviewer/ui/util/composable/LoadingBox.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
+
+const val TAG_LOADING_BOX = "loadingBoxTag"
 
 @Composable
 fun LoadingBox(
@@ -18,6 +21,7 @@ fun LoadingBox(
 ) {
     Box(
         modifier = modifier
+            .testTag(TAG_LOADING_BOX)
             .fillMaxSize()
             .padding(paddingValues),
         contentAlignment = Alignment.Center
